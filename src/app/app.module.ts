934adce2878733d2eb1e33bee4abee341939a70e
Elias Shalouha <eliasshalouhamadrid93@gmail.com>
2019-07-10 22:53:12 +0300

    Component 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -19,8 +22,11 @@ import { MatListModule } from '@angular/material/list';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule, 
-     MatListModule   
+    FlexLayoutModule,
+    MatListModule,  
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
